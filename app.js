481fff2a03c0ac872a0dd597261b972c79d623d4
Cyber Sam <hackspycyber@gmail.com>
2022-08-10 13:09:44 -0400

    committing all files

---
+++ app.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll('.link');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const modalCancelBtn = document.querySelector('.alt-btn button');
+
+links.forEach((link) => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        overlay.style.display = 'block';
+        modal.style.display = 'block';
+    });
+});
+
+modalCancelBtn.addEventListener('click', function () {
+    overlay.style.display = 'none';
+    modal.style.display = 'none';
+});
+
+overlay.addEventListener('click', function (e) {
+    e.target.style.display = 'none';
+    modal.style.display = 'none';
+});
